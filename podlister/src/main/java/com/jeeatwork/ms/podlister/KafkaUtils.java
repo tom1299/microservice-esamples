5a7f9f3e86455e4898c5e3272d3f72afa880a58d
tom1299 <tom1299@gmx.de>
2021-03-04 09:21:40 +0100

    Add consumer props and defaults for kafka

--- podlister/src/main/java/com/jeeatwork/ms/podlister/KafkaUtils.java
+++ podlister/src/main/java/com/jeeatwork/ms/podlister/KafkaUtils.java
@@ -5,12 +5,13 @@ package com.jeeatwork.ms.podlister;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ser.std.StringSerializer;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 
@@ -46,11 +47,11 @@ public class KafkaUtils {
 	public static Properties addBootstrapServer(Properties props) {
 		String brokerHost = System.getenv("BROKER_HOST");
 		if (brokerHost == null || brokerHost.isEmpty()) {
-			brokerHost = "172.18.0.2";
+			brokerHost = "172.18.0.3";
 		}
 		String brokerPort = System.getenv("BROKER_PORT");
 		if (brokerPort == null || brokerPort.isEmpty()) {
-			brokerPort = "30195";
+			brokerPort = "30101";
 		}
 		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerHost + ":" + brokerPort);
 		return props;
@@ -59,8 +60,23 @@ public class KafkaUtils {
 	public static Properties createProducerProps(Object usingObject) {
 		Properties props = KafkaUtils.addBootstrapServer(new Properties());
 		props.put(ProducerConfig.CLIENT_ID_CONFIG, usingObject.getClass().getSimpleName());
+		addSerdesProps(props);
+		log.info("Created kafka producer properties are %s", props);
+		return props;
+	}
+
+	private static void addSerdesProps(Properties props) {
 		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+	}
+
+	public static Properties createConsumerProps(String groupIdConfig) {
+		Properties props = KafkaUtils.addBootstrapServer(new Properties());
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+		props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, groupIdConfig);
 		log.info("Created kafka producer properties are %s", props);
 		return props;
 	}
